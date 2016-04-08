290dc4d308753a2ca203d4b97ee534a860098cdd
Emily Osowski & Noah Schutte & Patrick Skelley <pair+emosowski.noahschutte.pskelley@devbootcamp.com>
2016-04-08 15:20:57 -0400

    Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
-require_relative '../parseable'
 require_relative '../app/models/public_park_toilet'
+
+require 'open-uri'
+require 'JSON'
+
+$ruby_data = JSON.parse(open("https://data.cityofnewyork.us/resource/hjae-yuav.json").read)
 
 $ruby_data.each do |park|
   PublicParkToilet.create(park)
 end
+
